d4ed9a6972d20092b39216fee72f9c5025c058d6
Nthabi Mashiane (ZA) <nthabimashiane@gmail.com>
2020-04-26 17:11:56 +0200

    added code to fetch the data

---
+++ index.js
@@ -0,0 +1,7 @@
+const siteUrl = "https://remoteok.io/";
+const axios = require("axios");
+
+const fetchData = async() => {
+    const result = await axios.get(siteUrl);
+    return cheerio.load(esult.data);
+};
