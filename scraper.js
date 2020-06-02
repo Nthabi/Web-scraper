7f9320ed5e1a91bc4b3d9d59f788fe8e00204456
Nthabi Mashiane (ZA) <Nthabi.Mashiane@absa.africa>
2020-06-02 22:18:46 +0200

    functionality to scrape US president's names on wikipedia

---
+++ scraper.js
@@ -0,0 +1,20 @@
+const rp = require("request-promise");
+const url =
+  "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
+const $ = require("cheerio");
+
+rp(url)
+  .then(function(html) {
+    //successful scenario
+    const wikiUrls = [];
+    let docLength = $("b > a", html).length;
+    for (let i = 0; i < docLength; i++) {
+      wikiUrls.push($("b > a", html)[i].attribs.href);
+    }
+    // console.log("====================================");
+    // console.log(wikiUrls);
+    // console.log("====================================");
+  })
+  .catch(function(err) {
+    //handle error
+  });
